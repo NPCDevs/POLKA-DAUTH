419e135cde623d08bc0fea451c9941ce75d64302
IvanSeagull <ivan.seagull.k@gmail.com>
2023-06-24 17:24:23 +0100

    wip

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -5,12 +5,16 @@ import '../styles/globals.css';
 import { Astar } from '@thirdweb-dev/chains';
 import MainLayout from '../src/components/global/Layout';
 
+import { MantineProvider } from '@mantine/core';
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider activeChain={Astar}>
-      <MainLayout>
-        <Component {...pageProps} />
-      </MainLayout>
+      <MantineProvider withGlobalStyles withNormalizeCSS>
+        <MainLayout>
+          <Component {...pageProps} />
+        </MainLayout>
+      </MantineProvider>
     </ThirdwebProvider>
   );
 }
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,9 +1,16 @@
-import { ConnectWallet } from '@thirdweb-dev/react';
+import { ConnectWallet, useAddress } from '@thirdweb-dev/react';
 import type { NextPage } from 'next';
 import styles from '../styles/Home.module.css';
+import MainPage from '../src/components/MainPage';
 
 const Home: NextPage = () => {
-  return <div className={styles.container}></div>;
+  const address = useAddress();
+
+  if (address && address.length > 0) {
+    return <MainPage />;
+  }
+
+  return <h1>123</h1>;
 };
 
 export default Home;
